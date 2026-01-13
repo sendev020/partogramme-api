827b59ad8cf58b711cff38160b9d2aad07079dac
SENE-Dev <71398525+SENE-Dev@users.noreply.github.com>
2026-01-13 11:18:28 +0000

    Fix UserFactory: force Faker injection

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,9 +2,11 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Faker\Generator as Faker;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -23,11 +25,14 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        /** @var Faker $faker */
+        $faker = app(Faker::class);
+
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'name' => $faker->name(),
+            'email' => $faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
     }
